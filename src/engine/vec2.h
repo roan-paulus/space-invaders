176e8cd454ef563abffa486498cd328a6f6262d2
roan-paulus <roanpaulus@proton.me>
2026-09-24 15:47:11 +0200

    add vector operations

--- src/engine/vec2.h
+++ src/engine/vec2.h
@@ -4,6 +4,27 @@
 struct Vec2 {
     float x;
     float y;
+
+    Vec2 operator+(Vec2 b) {
+        return {
+            .x = x + b.x,
+            .y = y + b.y
+        };
+    }
+
+    Vec2 operator-(Vec2 b) {
+        return {
+            .x = x - b.x,
+            .y = y - b.y 
+        };
+    }
+
+    Vec2 operator*(Vec2 b) {
+        return {
+            .x = x * b.x,
+            .y = y * b.y
+        };
+    }
 };
 
 #endif
